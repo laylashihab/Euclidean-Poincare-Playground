53d50b7749da452e0c3ddc2d6d651c27396f2f25
laylashihab <lshihab@purdue.edu>
2025-05-21 15:36:01 -0700

    basic setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Topology
+
+#### This is intended to be a project to play around with visualizations of topological spaces
---
+++ mobiusStrip.py
@@ -0,0 +1,4 @@
+# file to visualize a mobius strip in 3 dimensional space using matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
