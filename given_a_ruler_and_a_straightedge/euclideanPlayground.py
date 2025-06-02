777b11a83b27dfaf3d409e9fa3cb07f80723048a
laylashihab <lshihab@purdue.edu>
2025-06-02 11:30:26 -0700

    began setting up circle functionality

--- given_a_ruler_and_a_straightedge/euclideanPlayground.py
+++ given_a_ruler_and_a_straightedge/euclideanPlayground.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+import matplotlib.patches as patches
+import math        
+
 
 class Point:
     __x = 0
@@ -44,7 +47,7 @@ class Line:
     def __init__(self):
         pointList[self] = []
 
-    def plotLine(self):
+    def plotShape(self):
         if self.__endPoint != None:
             x_data = np.linspace(self.__startPoint.getX(), self.__endPoint.getX(), 100)
             y_data = np.linspace(self.__startPoint.getY(), self.__endPoint.getY(),100)
@@ -56,7 +59,7 @@ class Line:
             self.__endPointPlot, = plot1.plot(self.__endPoint.getX(),self.__endPoint.getY(), "o")
             canvas.draw()
     
-    def removeLine(self):
+    def removeShape(self):
         if self.__endPointPlot != None:
             # removes line and endPoint
             self.__line.remove()
@@ -84,8 +87,56 @@ class Line:
     def getEndPoint(self):
         return self.__endPoint
     
-    def getLine(self):
+    def getShape(self):
         return self.__line
+    
+class Circle:
+    __circle = None
+    __centerPoint = None
+    __radius = None
+
+    __centerPointPlot = None
+    
+
+    def __init__(self):
+        pointList[self] = []
+
+    def plotShape(self):
+        if self.__centerPoint != None:
+            self.__circle = patches.Circle((self.__centerPoint.getX(), self.__centerPoint.getY()), radius=self.__radius, edgecolor='blue', facecolor = 'None')
+
+            #plots circle and radius point
+            plot1.add_patch(self.__circle)
+            self.__centerPointPlot, = plot1.plot(self.__centerPoint.getX(),self.__centerPoint.getY(), "o")
+            canvas.draw()
+
+    def removeShape(self):
+        if self.__circle != None:
+            # removes line and endPoint
+            self.__circle.remove()
+            self.__centerPointPlot.remove()
+            canvas.draw()
+
+    # sets the center point
+    def setStartPoint(self, centerPoint):
+        self.__centerPoint = centerPoint
+        if (len(pointList[self]) == 0):
+            pointList[self].append(self.__centerPoint)
+        else:
+            pointList[self][0] = self.__centerPoint
+
+    def setEndPoint(self, endPoint):
+        self.__radius = math.sqrt(((self.__centerPoint.getX()-endPoint.getX()))**2+(self.__centerPoint.getY()-endPoint.getY())**2)
+    
+    def getCenterPoint(self):
+        return self.__centerPoint
+    
+    def getRadius(self):
+        return self.__radius
+    
+    def getShape(self):
+        return self.__circle
+
 
 """
 Series of functions to deal with mouseEvents
@@ -93,50 +144,63 @@ Series of functions to deal with mouseEvents
 """
 # function to deal with user clicking.
 def click_handler(event):
+    global mouseDown
+    global currentShape
     global movePoint
-    currentPoint = Point(event.x,plot_size- event.y)
+    currentPoint = Point(event.xdata, event.ydata)
+    mouseDown = True
+
+    if (not event.inaxes):
+        return
 
     # checks if user is clicking on an existing point
     for object, points in pointList.items():
         startPoint = points[0]
         endPoint = points[1]
         if (currentPoint.equals(startPoint)):
-            object.setStartPoint(endPoint)
-            object.setEndPoint(startPoint)
+            # flips start and end point of the line
+            if (mode == "Line"):
+                object.setStartPoint(endPoint)
+                object.setEndPoint(startPoint)
             
             movePoint = currentPoint
+            currentShape = object
             return
         elif (currentPoint.equals(endPoint)):
             movePoint = currentPoint
+            currentShape = object
             return
-
-    global currentLine
-    currentLine = Line()
-    currentLine.setStartPoint(currentPoint)
-    lineList.append(currentLine)
+    currentShape = Line() if mode == "Line" else Circle()
+    currentShape.setStartPoint(currentPoint)
+    shapeList.append(currentShape)
 
 def drag_handler(event):
-    global movePoint
-    currentPoint = Point(event.x,plot_size- event.y)
+    if (not event.inaxes):
+        return
+    if mouseDown:
+        global movePoint
+        currentPoint = Point(event.xdata,event.ydata)
 
-    if (movePoint != None):
-        movePoint = currentPoint
-    #removes the current drawing of the line
-    currentLine.removeLine()
-    currentLine.setEndPoint(currentPoint)
-    currentLine.plotLine()
+        if (movePoint != None):
+            movePoint = currentPoint
+        #removes the current drawing of the line
+        currentShape.removeShape()
+        currentShape.setEndPoint(currentPoint)
+        currentShape.plotShape()
 
 def unclick_handler(event):
-    pass
+    global mouseDown
+    mouseDown = False
 
 # changes type of shape plot
 def changeMode(newMode):
+    global mode
     mode = newMode
 
 # clears the plot
 def clear():
-    global lineList
-    lineList = []
+    global shapeList
+    shapeList = []
     plot1.cla()
     plot1.set_xlim(0,plot_size)
     plot1.set_ylim(0,plot_size)
@@ -147,16 +211,17 @@ def clear():
 startPoint = [0,0]
 mode = "Line"
 plot_size = 400
-currentLine = None
+currentShape = None
 movePoint = None
 epsilon = 5 #amount of error allowed for clicking on points
+mouseDown = False
 
 # store various graph objects
-lineList = [] 
+shapeList = [] 
 pointList = {} #dictionary to store points based on their associated structures (lines, circles, etc)
 
 # the figure that will contain the plot
-fig = Figure(figsize = (5, 5), dpi = 100)
+fig = Figure(figsize = (5, 5), dpi = 100, constrained_layout=True)
 
 #main window setup
 root = Tk()
@@ -169,8 +234,10 @@ descriptLabel.pack()
 
 # tool setup
 toolLabel = Label(root, text="Tools:")
-lineButton = Button(root, command = changeMode("Line"), height = 2, width = 10, text = "Line")
+lineButton = Button(root, command=lambda: changeMode("Line"), height = 2, width = 10, text = "Line")
 lineButton.pack()
+circleButton = Button(root, command =lambda: changeMode("Circle"), height = 2, width = 10, text = "Circle")
+circleButton.pack()
 clearButton = Button(root,command=clear,height = 2, width = 10, text = "Clear")
 clearButton.pack()
 
@@ -196,8 +263,8 @@ toolbar.update()
 canvas.get_tk_widget().pack()
 
 # bindings
-root.bind("<Button>", click_handler)
-root.bind("<B1-Motion>", drag_handler)
-root.bind("<ButtonRelease>", unclick_handler)
+canvas.mpl_connect("button_press_event", click_handler)
+canvas.mpl_connect("motion_notify_event", drag_handler)
+canvas.mpl_connect("button_release_event", unclick_handler)
 
 root.mainloop()
